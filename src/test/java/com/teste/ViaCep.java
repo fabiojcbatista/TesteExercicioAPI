d84f501c78e95f73c8b7b4999419ddbe4a79040f
Fabio Jr <fabiojcbweb@gmail.com>
2022-04-19 19:26:36 -0300

    Criação de testes ViaCep

---
+++ src/test/java/com/teste/ViaCep.java
@@ -0,0 +1,53 @@
+package com.teste;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+
+public class ViaCep {
+
+    @BeforeClass
+    public static void urlbase() {
+        RestAssured.baseURI = "https://viacep.com.br/ws/";
+    }
+
+    @Test
+    public void dadoViaCepComCep94440360_QuandoBuscarDados_EntaoDeveTerDDD51() {
+        given().contentType("application/json")
+                .pathParam("cep", "94440360")
+                .when()
+                .get("{cep}/json")
+                .then()
+                .assertThat()
+                .body("ddd", equalTo("51"))
+                .statusCode(200)
+                .contentType("application/json");
+    }
+    @Test
+    public void dadoViaCepComCep94440361_QuandoBuscarDados_EntaoDeveRetornarErrTrue() {
+        given().contentType("application/json")
+                .pathParam("cep", "94440361")
+                .when()
+                .get("{cep}/json")
+                .then()
+                .assertThat()
+                .body("erro", equalTo("true"))
+                .statusCode(200)
+                .contentType("application/json");
+    }
+
+    @Test
+    public void dadoViaCepComCep9444036o_QuandoBuscarDados_EntaoDeveRetornarStatusCode400() {
+        given().contentType("application/json")
+                .pathParam("cep", "9444036o")
+                .when()
+                .get("{cep}/json")
+                .then()
+                .assertThat()
+                .statusCode(400);               
+    }
+}
